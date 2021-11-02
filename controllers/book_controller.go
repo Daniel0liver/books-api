7d26f944c9d574d5f61368156fb0db8d15a1f8a2
Daniel Oliveira <danieloliver.dev@gmail.com>
2021-11-02 19:25:58 -0300

    feat: create book controller

---
+++ controllers/book_controller.go
@@ -0,0 +1,23 @@
+package controllers
+
+import "github.com/gin-gonic/gin"
+
+func List(c *gin.Context) {
+	c.JSON(200, gin.H{})
+}
+
+func FindById(c *gin.Context) {
+	c.JSON(200, gin.H{})
+}
+
+func Create(c *gin.Context) {
+	c.JSON(200, gin.H{})
+}
+
+func Update(c *gin.Context) {
+	c.JSON(200, gin.H{})
+}
+
+func Delete(c *gin.Context) {
+	c.JSON(200, gin.H{})
+}
